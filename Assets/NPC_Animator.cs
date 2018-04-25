72a4c3e8a3de92da5ad3af6020c7ddc91ba967eb
Jordan Phoenix <jordan.phoenix1993@gmail.com>
2018-04-25 22:18:21 +0100

    Initial submit of project files.

---
+++ Assets/NPC_Animator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC_Animator : MonoBehaviour
+{
+    //Animator
+    private Animator _animator;
+
+    //Animator Controller variables.
+    public bool isWalking;
+    public float walkDirectionVertical;
+
+	// Use this for initialization
+	void Start ()
+    {
+        //Get Animator Component
+        _animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //Update Animator Controller Variables
+        _animator.SetBool("IsWalking", isWalking);
+        _animator.SetFloat("WalkDirectionVertical", walkDirectionVertical);
+	}
+}
---
+++ Assets/SpriteBillboard.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteBillboard : MonoBehaviour
+{
+    //Player Camera
+    private GameObject playerCamera;
+
+	// Use this for initialization
+	void Start ()
+    {
+        //Find player camera
+        playerCamera = GameObject.FindGameObjectWithTag("MainCamera");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //Set Look At target based on player camera but ignoring Y-Axis
+        Vector3 targetPostition = new Vector3(  playerCamera.transform.position.x,
+                                                transform.position.y,
+                                                playerCamera.transform.position.z
+                                                );
+
+        //Look at target
+        transform.LookAt(targetPostition);
+    }
+}
